9f7f1e7eafc9ae3238ee0c8b713f3e60c91fe417
ItsJuniorDias <juniordias_@live.com>
2020-05-07 19:32:54 -0300

    Create List with Column Widget

--- studentbook/lib/main.dart
+++ studentbook/lib/main.dart
@@ -10,7 +10,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: <Widget> [
+           Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100.0'),
+              subtitle: Text('1000'),
+            )
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('200.0'),
+              subtitle: Text('1000'),
+            )
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('500.0'),
+              subtitle: Text('1000'),
+            )
+          ),
+          ],
+        ),
         appBar: AppBar(title: Text('Home'),),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
       )
     );
   }
