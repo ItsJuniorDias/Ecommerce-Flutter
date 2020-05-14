3bb1aa55bbf6c4b4215f7ae9ff01d4255d011e69
ItsJuniorDias <juniordias_@live.com>
2020-05-14 00:16:41 -0300

    Create Widget and replete

--- studentbook/lib/pages/Catalogo.dart
+++ studentbook/lib/pages/Catalogo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/CardProduct.dart';
 import '../widgets/ItemCategories.dart';
 
 class Catalogo extends StatelessWidget {
@@ -54,110 +55,11 @@ class Catalogo extends StatelessWidget {
             child: Flexible(
                 child: ListView(
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.fromLTRB(16, 16, 16, 16),
-                  width: 80,
-                  height: 125,
-                    decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                       boxShadow: [
-                        BoxShadow(
-                          color: Color.fromRGBO(0, 0, 0, 0.12),
-                          offset: Offset(0,10),
-                          blurRadius: 12,
-                          )
-                        ]
-                    ),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                          width: 67,
-                          height: 67,
-                          margin: EdgeInsets.fromLTRB(16, 16, 16, 16),
-                          decoration: BoxDecoration(
-                             boxShadow: [
-                                BoxShadow(
-                                  color: Color.fromRGBO(0, 0, 0, 0.19),
-                                  offset: Offset(0,10),
-                                  blurRadius: 12,
-                        ),
-                                    ]
-                          ),
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                "https://avatars1.githubusercontent.com/u/50254416?s=460&v=4"),
-                          )),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Container(
-                            width: 320,
-                            height: 105,
-                            margin: EdgeInsets.fromLTRB(10, 10,10, 10),
-                            padding: EdgeInsets.fromLTRB(10, 10,10, 10),
-                            decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(15))),
-                            child: Column(
-                               crossAxisAlignment: CrossAxisAlignment.start,
-                               children: <Widget>[
-                                 Container(
-                                  padding: const EdgeInsets.only(bottom: 5),
-                                  child: Text(
-                                    'Pullover',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                                Text(
-                                  'Mango',
-                                  style: TextStyle(
-                                    color: Colors.grey[500],
-                                  ),
-                                ),
-                                Container(
-                                  padding: const EdgeInsets.only(bottom: 5),
-                                  child: Row(
-                                    children: <Widget>[
-                                      Icon(
-                                        Icons.star,
-                                        color: Colors.red[500],
-                                      ),
-                                       Icon(
-                                        Icons.star,
-                                        color: Colors.red[500],
-                                      ),
-                                       Icon(
-                                        Icons.star,
-                                        color: Colors.red[500],
-                                      ),
-                                       Icon(
-                                        Icons.star,
-                                        color: Colors.red[500],
-                                      ),
-                                       Icon(
-                                        Icons.star,
-                                        color: Colors.red[500],
-                                      ),
-                                   ],
-                                  ),
-                                ),
-                                Text(
-                                    'RS 51,00',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                               ],
-                            ),                              
-                          ),
-                        ],
-                      )
-                    ],
-                  )
-                ),
+                 CardProduct(),
+                 CardProduct(),
+                 CardProduct(),
+                 CardProduct(),
+                 CardProduct(),
               ],
             )),
           )
---
+++ studentbook/lib/widgets/CardProduct.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class CardProduct extends StatelessWidget{
+  final String name;
+  final String subname;
+  final String value;
+  final String stars;
+
+  @override build(BuildContext context) {
+    return Container(
+                  margin: EdgeInsets.fromLTRB(16, 16, 16, 16),
+                  width: 80,
+                  height: 125,
+                    decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                       boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.12),
+                          offset: Offset(0,10),
+                          blurRadius: 12,
+                          )
+                        ]
+                    ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          width: 67,
+                          height: 67,
+                          margin: EdgeInsets.fromLTRB(16, 16, 16, 16),
+                          decoration: BoxDecoration(
+                             boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromRGBO(0, 0, 0, 0.19),
+                                  offset: Offset(0,10),
+                                  blurRadius: 12,
+                        ),
+                                    ]
+                          ),
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://avatars1.githubusercontent.com/u/50254416?s=460&v=4"),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            width: 320,
+                            height: 105,
+                            margin: EdgeInsets.fromLTRB(10, 10,10, 10),
+                            padding: EdgeInsets.fromLTRB(10, 10,10, 10),
+                            decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(15))),
+                            child: Column(
+                               crossAxisAlignment: CrossAxisAlignment.start,
+                               children: <Widget>[
+                                 Container(
+                                  padding: const EdgeInsets.only(bottom: 5),
+                                  child: Text(
+                                    'Pullover',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  'Mango',
+                                  style: TextStyle(
+                                    color: Colors.grey[500],
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.only(bottom: 5),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.star,
+                                        color: Colors.red[500],
+                                      ),
+                                       Icon(
+                                        Icons.star,
+                                        color: Colors.red[500],
+                                      ),
+                                       Icon(
+                                        Icons.star,
+                                        color: Colors.red[500],
+                                      ),
+                                       Icon(
+                                        Icons.star,
+                                        color: Colors.red[500],
+                                      ),
+                                       Icon(
+                                        Icons.star,
+                                        color: Colors.red[500],
+                                      ),
+                                   ],
+                                  ),
+                                ),
+                                Text(
+                                    'RS 51,00',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                               ],
+                            ),                              
+                          ),
+                        ],
+                      )
+                    ],
+                   )
+                  );
+                
+  }
+}
